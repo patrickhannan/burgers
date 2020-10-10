062d8d23744d52d89d6f5402d4ce2171d946e398
Patrick Hannan <patrick@hannan.com>
2020-10-09 22:56:26 -0400

    Based orm on cats app from gt-atl repo.

--- config/orm.js
+++ config/orm.js
@@ -1,33 +1,93 @@
-const connection = require("./connection")
 
-const connection = require("./connection");
+var connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+
+  return arr.toString();
+}
 
 var orm = {
-    selectAll: function (table, cb) {
-      connection.query(`SELECT * FROM ??`, [table], function (err, result) {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    },
-    insertOne: function (table, burgerName, cb) {
-      connection.query(`INSERT INTO ?? (burger_name) VALUES (?)`, [table, burgerName], function (err, result) {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    },
-    updateOne: function (table, id, cb) {
-      connection.query(`UPDATE ?? SET devoured = true
-      WHERE id = ?`, [table, id], function (err, result) {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    },
-  };
+  selectAll: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
 
-  module.exports = orm;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+
+  updateOne: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  delete: function (table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+};
+
+module.exports = orm;
