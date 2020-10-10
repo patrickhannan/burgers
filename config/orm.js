05a8d27bd06d33f0a3f2049271cda245ab0b7d5c
Patrick Hannan <patrick@hannan.com>
2020-10-09 22:46:19 -0400

    added selectAll, insertOne, updateOne functions.

--- config/orm.js
+++ config/orm.js
@@ -2,3 +2,30 @@ const connection = require("./connection")
 
 const connection = require("./connection");
 
+var orm = {
+    selectAll: function (table, cb) {
+      connection.query(`SELECT * FROM ??`, [table], function (err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    insertOne: function (table, burgerName, cb) {
+      connection.query(`INSERT INTO ?? (burger_name) VALUES (?)`, [table, burgerName], function (err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    updateOne: function (table, id, cb) {
+      connection.query(`UPDATE ?? SET devoured = true
+      WHERE id = ?`, [table, id], function (err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+  };
